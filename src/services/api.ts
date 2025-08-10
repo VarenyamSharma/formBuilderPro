6e89d2b4429e323cc321243ef79b690fee35b109
varenyam Sharma <varenyamsharma12@gmail.com>
2025-08-10 12:41:17 +0530

    Fix MongoDB and API connection errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { Form, Submission } from '../types/form';
 
-const API_BASE_URL = 'https://form-builder-pro-kkl9.vercel.app/api';
-// const API_BASE_URL = 'https://form-builder-pro-beta.vercel.app/api';
+const API_BASE_URL = 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
